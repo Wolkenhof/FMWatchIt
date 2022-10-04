bb465ff7fdb3926c6a038252b3c97923bac67d2b
Jonas Günner <jonas@exploitox.de>
2022-10-04 20:56:06 +0200

    PoC of service watcher

---
+++ FMFuckIt/MailReport.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Webhook;
+
+namespace FMFuckIt
+{
+    internal class MailReport
+    {
+        public static void SendFullReport(string ConfigFilePath, string LatestLogFilePath)
+        {
+            Console.WriteLine("Creating report ...");
+            DiscordWebhook hook = new DiscordWebhook();
+            hook.Url = "ZENSIERT";
+
+            var http = new System.Net.WebClient();
+            var ip = http.DownloadString("https://ifconfig.me/ip)");
+            var from = new MailAddress("ZENSIERT");
+            var to = new MailAddress("ZENSIERT");
+            var subject = $"FMFuckIt Report - {ip}";
+            var body = "A report was created.";
+
+            var username = "ZENSIERT"; // get from Mailtrap
+            var password = "ZENSIERT"; // get from Mailtrap
+
+            var host = "ZENSIERT";
+            var port = 465;
+
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+            
+            var mail = new MailMessage();
+            mail.Subject = subject;
+            mail.From = from;
+            mail.To.Add(to);
+            mail.Body = body;
+
+            var configAttachment = new Attachment(ConfigFilePath);
+            var logAttachment = new Attachment(LatestLogFilePath);
+            mail.Attachments.Add(configAttachment);
+            mail.Attachments.Add(logAttachment);
+
+            client.Send(mail);
+
+            Console.WriteLine("Email sent!");
+        }
+    }
+}
---
+++ FMFuckIt/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * FMFuckIt - A fucking service watcher for FMAudit (ECI DCA)
+ * ---
+ * Copyright (c) 2018 - 2022 valnoxy. All rights reserved.
+ */
+
+using System.ServiceProcess;
+
+namespace FMFuckIt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FMFuckIt - A fucking service watcher for FMAudit (ECI DCA)");
+            Console.WriteLine("Copyright (c) 2018 - 2022 valnoxy. All rights reserved.");
+            Console.WriteLine("----------------------------------------------------------");
+
+            ExtendedServiceController xServiceController = new ExtendedServiceController(Configuration.ServiceName);
+            xServiceController.StatusChanged += xServiceController_StatusChanged;
+            Console.Read();
+        }
+
+        // This event handler will catch service status changes externally as well
+        private static void xServiceController_StatusChanged(object sender, ServiceStatusEventArgs e)
+        {
+            Console.WriteLine("Status Changed: " + e.Status);
+            if (e.Status == ServiceControllerStatus.Stopped)
+            {
+                Console.WriteLine("Service stopped, restarting...");
+                ExtendedServiceController xServiceController = new ExtendedServiceController(Configuration.ServiceName);
+                xServiceController.Start();
+                xServiceController.WaitForStatus(ServiceControllerStatus.Running);
+                Console.WriteLine("Service restarted.");
+                MailReport.SendFullReport("C:\\ProgramData\\ECI DCA\\dca.config", "C:\\ProgramData\\ECI DCA\\dca.log");
+            }
+        }
+    }
+}
